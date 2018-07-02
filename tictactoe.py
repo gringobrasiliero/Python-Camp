4f98ec8e72ea7c6abf396320cd027d28583c9b4a
Nolan Byrnes <nolanehs@gmail.com>
2018-07-02 11:28:44 -0400

    Board and Place array made

---
+++ tictactoe.py
@@ -0,0 +1,12 @@
+place = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+print(place)
+
+print(place[0])
+
+def board():
+    print("___|___|___")
+    print("___|___|___")
+    print("   |   |   ")
+
+# board()
