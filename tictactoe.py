f0528b2971a33dbcec20951e56780fb4195ef39f
Nolan Byrnes <nolanehs@gmail.com>
2018-07-03 11:30:40 -0400

    Working on A.I.

--- tictactoe.py
+++ tictactoe.py
@@ -4,15 +4,23 @@ place = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 win_combo = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [6,4,2], [0,4,8]]
 
+def possibleWin():
+    for i in win_combo:
+        if place[i[0]] == place[i[1]] or place[i[1]] == place[i[2]] or place[i[0]] == place[i[2]]:
+            for c in i:
+                if place[c] != "X" and place[c] != "O":
+                    place[c] = "O"
+
+
+# possibleWin()
+
 def win():
     for i in win_combo:
         if place[i[0]] == place[i[1]] == place[i[2]] == "X":
             print( "Human Wins!")
-            game()
             again()
-        elif place[i[0]] == place[i[1]] == place[i[2]] == "X":
+        elif place[i[0]] == place[i[1]] == place[i[2]] == "O":
             print("CPU Wins!")
-            game()
             again()
 def again():
     rematch = input("Would you like to play again? (y or n) ")
@@ -34,22 +42,30 @@ def human_move():
         place[move] = "X"
 
 def cpu_move():
-    cpuMove = randint(0,8)
-    if place[cpuMove] == "X" or place[cpuMove] == "O":
-        cpu_move()
-    else:
-        place[cpuMove] = "0"
+    # cpuMove = randint(0,8)
+    possibleWin()
+    cpu_choice = [4, 8, 0, 2, 6, 3, 5, 7, 1]
+    for i in cpu_choice:
+        if place[i] != "X" and place[i] != "O":
+            place[i] = "O"
+            break
+
 
 def board():
     print("_" + place[6] + "_|_" + place[7] + "_|_" + place[8] + "_")
     print("_" + place[3] + "_|_" + place[4] + "_|_" + place[5] + "_")
     print(" " + place[0] + " | " + place[1] + " | " + place[2] + " ")
+    print("")
 
 
 def game():
-
+    global place
+    place = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
     while True:
         board()
         human_move()
+        win()
         cpu_move()
-# game()
+        board()
+        win()
+game()
