78e850478974186214518358d5c7e660655e1a21
Nolan Byrnes <nolanehs@gmail.com>
2018-07-06 09:21:44 -0400

    Intro and game function created

---
+++ rpg.py
@@ -0,0 +1,17 @@
+
+
+def intro():
+    name = input("I am grateful you are willing to go on this epic adventure. What is your name? ")
+    print("======================================================================")
+    print("Well " + name + " , take this. It is dangerous to go alone. ")
+    print("======================================================================")
+    print("======================================================================")
+
+
+
+
+
+def game():
+    intro()
+
+game()
