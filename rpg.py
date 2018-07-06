b37ab3f8ca8530f0c331bcb7a3f81c38688b9b4d
Nolan Byrnes <nolanehs@gmail.com>
2018-07-06 09:43:38 -0400

    Enemy made

--- rpg.py
+++ rpg.py
@@ -6,12 +6,103 @@ def intro():
     print("Well " + name + " , take this. It is dangerous to go alone. ")
     print("======================================================================")
     print("======================================================================")
+    print("""
+              _______
+         ..-'`       ````---.
+       .'          ___ .'````.'SS'.
+      /        ..-SS####'.  /SSHH##'.
+     |       .'SSSHHHH##|/#/#HH#H####'.
+    /      .'SSHHHHH####/||#/: \SHH#####\.
+   /      /SSHHHHH#####/!||;`___|SSHH###\.
+-..__    /SSSHHH######.         \SSSHH###\.
+`.'-.''--._SHHH#####.'           '.SH####/
+  '. ``'-  '/SH####`/_             `|H##/
+  | '.     /SSHH###|`'==.       .=='/\H|
+  |   `'-.|SHHHH##/\__\/        /\//|~|/
+  |    |S#|/HHH##/             |``  |
+  |    \H' |H#.'`              \    |
+  |        ''`|               -     /
+  |          /H\          .----    /
+  |         |H#/'.           `    /
+  |          \| | '..            /
+  |    ^~DLF   /|    ''..______.'
+   \          //\__    _..-. | 
+    \         ||   ````     \ |_
+     \    _.-|               \| |_
+     _\_.-'   `'''''-.        |   `--.
+ ''``    \            `''-;    \ /
+          \      .-'|     ````.' -
+          |    .'  `--'''''-.. |/
+          |  .'               \|
+          |.'
+    """)
 
 
+def enemy():
+    print("A wild Arbok Appeared")
+    print("""                    =HHH=                          
+                =HH=     =HH=                      
+            =HH=             =HH=                  
+         =H=       =HHHHH=       =H=               
+       =H       =H=       =H=       =H             
+      H        =             =        =            
+     =                                 H           
+     H           H          H          =           
+    H   HH==      H        H       HH   H          
+   H   H==HH= =   H=      =H  =  HH==H   H         
+  =   H=====H=   HHH      HHH  =H=====H   H        
+  H  H=======H    =H      H=   H=======H  =        
+ =  H========H==             = H========H  =       
+ H H==========H=H    H   H  H H==========H H       
+=  ====HH======H=H         H H======HH====  =      
+H  =====HH=====H===HHHHHHH=  H=====HH=====  H      
+H  =====HHHH====H==         H====HHHH=====  H      
+H   ====HH=HHH==HH===     =HH==HHH=HH====   H      
+H   ====HH= =HHH=HHHHHHHHHHH=HHH= =HH====   H      
+=    ===HH== ==HH=HHHHHHHHH=HH== ==HH===    =      
+ H   ====HH=  ==HH=HHHHHHH=HH==  =HH====   H       
+ =     ==HH==  =HH===HHH===HH=  ==HH==     =       
+  H     ==HH====HH=========HH====HH==     H        
+   H     ==HHHHHH===========HHHHHH==     H         
+    H     ====HHH===HHHHH===HHH====     H          
+     =H=== =========HHHHH========H    =H           
+       =H====H=====HHHHHHH=====HH=  =H             
+         =H===HHHHHHHHHHHHHHHHHH===H          =H   
+           H====HHHHHHHHHHHHHH===H           =  H  
+            =H====HHHHHHHHHH===HH           H  =   
+              HH====HHHHHH====H             =  H   
+                H====HHH=====H             =HH=H   
+                 H===== =====H             H   H   
+                 H====   ====H             H   H   
+                 H===    ====H             H  ==H  
+              =HHH===HH   ===H             HH=  =  
+           =H=   HH=   ==HH==H             =H    H 
+         =H H   ===        =HH              =   HH 
+       =H   H  =H=          ===             HHH=  =
+      H      H===            =HH            =     H
+     =      =H==             =H =H         H H    H
+    =      ===HHHHHHH=H      =H   =H      H   HH  =
+    H     ====H=       =H    =H=   HH   H= H    HH 
+   ==    ====H           HH  ==== H  =H=   H     = 
+   H==  HHH=H              H=====H     H    H  =H  
+   H===  H   =H=            HH===H==   H     =HH   
+   ===== H      =H         ==H==HH==   H    ==H    
+    H===H         HH       ==H==H======H=====H     
+     H=H=           H     ==H===H=======H===H      
+      H====          =   ==H  =HH========H=        
+       =H=====       =====H       =HHHH=           
+         =H===========HH=                          
+           =HHHHHHHH=
+""")
+
+
+def human_move():
+    print("======================================================================")
+    
 
 
 
 def game():
     intro()
-
+    enemy()
 game()
