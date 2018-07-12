083f1793e72b7f37d48153459fe37a5c043c5bda
Nolan Byrnes <nolanehs@gmail.com>
2018-07-12 19:13:22 -0400

    Explaining OO

--- Python Camp July 9/hello.py
+++ Python Camp July 9/hello.py
@@ -1,17 +1,20 @@
 
 name = input("What is your name? ")
 
+thirsty = input("Hello " + name + ", are you thirsty?")
 
+if thirsty == "yes":
+    print("Go get some water!")
+elif thirsty == "no":
+    print("Okay then.")
+else:
+    print("I do not understand you. Please answer yes or no")
 
-def areYouThirsty():
-    thirsty = input("Hello " + name + ", are you thirsty?")
+hungry = input("Hello " + name + ", are you hungry?")
 
-    if thirsty == "yes":
-        print("Go get some water!")
-    elif thirsty == "no":
-        print("Okay then.")
-    else:
-        print("I do not understand you. Please answer yes or no")
-        areYouThirsty()
-
-areYouThirsty()
+if hungry == "yes":
+    print("Go get some food!")
+elif hungry == "no":
+    print("Okay then.")
+else:
+    print("I do not understand you. Please answer yes or no")
