60810d7c44443b948e991c54b16e24384f91d951
Nolan Byrnes <nolanehs@gmail.com>
2018-07-12 19:12:32 -0400

    Code Challenge

---
+++ Students/challenge.py
@@ -0,0 +1,2 @@
+letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'DGHTTHRTHPDGH;LMCFBLM;CFBPOMCFB;OL,XFB;LO,DCXV,LVVM
+JH]
