a605951b004f6337b76036881e001447b63e455d
Nolan Byrnes <nolanehs@gmail.com>
2018-07-25 09:14:34 -0400

    Python Basics

---
+++ Python Camp July 23/intro.py
@@ -0,0 +1,69 @@
+#
+# name = input("What is your name?")
+#
+#
+#
+# def thirsty():
+#     thirst = input("Hello " + name + ", are you thirsty?")
+#
+#     if thirst == "y":
+#         print("Go get some water.")
+#     elif thirst == "n":
+#         print("Okay.")
+#     else:
+#         print("What?")
+#         thirsty()
+#
+# thirsty()
+
+#####################################################
+
+
+def stairs():
+    height = int(input("How High?"))
+    block = "[]"
+    x=1
+    while x <= height:
+        print(block)
+        block = block + "[]"
+        x = x + 1
+
+word = "spaghetti"
+
+for i in word:
+    print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
